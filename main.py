5f9746087ea39b3e8e8eb805adedbdeb1fbd4b3d
lincoln ren <rql7777@icloud.com>
2026-01-31 11:11:37 -0500

    Initial FastAPI backend

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from routers import news
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+#挂载路由/注册路由
+app.include_router(news.router)
